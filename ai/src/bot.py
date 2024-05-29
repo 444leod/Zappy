5a1f60b04e78fe2440b0c97c30bcdf739ca46596
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-05-29 16:19:47 +0200

    feat(ServerConnection): add Config class to handle CL args

--- ai/src/bot.py
+++ ai/src/bot.py
@@ -3,6 +3,35 @@
 import socket
 import sys
 
+USAGE = "USAGE: ./zappy_ai -p port -n name -h machine"
+
+class Config:
+    def __init__(self):
+        argv = sys.argv
+        if (len(argv) == 1):
+            print(USAGE)
+            sys.exit(84)
+        if (argv[1] == "--help"):
+            print(USAGE)
+            sys.exit(0)
+        try:
+            self.port: int = int(argv[argv.index("-p") + 1])
+            self.name: str = argv[argv.index("-n") + 1]
+        except Exception as _:
+            print(USAGE)
+            sys.exit(84)
+        try:
+            self.machine: str = argv[argv.index("-h") + 1]
+        except ValueError:
+            self.machine = "localhost"
+        except IndexError:
+            print(USAGE)
+            sys.exit(84)
+        if (self.name == "GRAPHIC"):
+            print("Name can't be 'GRAPHIC' as it's reserved for the GUI.")
+            print(USAGE)
+            sys.exit(84)
+
 class CommandHandler():
     def __init__(self, hostname: str = "localhost", port: int = 5555):
         self.hostname = hostname
@@ -11,7 +40,7 @@ class CommandHandler():
         self.sock.connect((self.hostname, self.port))
     
     def send_command(self, command: str) -> None:
-        self.sock.send(command.encode())
+        self.sock.send((command + "\n").encode())
 
     def receive_response(self) -> str:
         buff = ""
@@ -20,18 +49,22 @@ class CommandHandler():
         return buff
 
 def main():
-    handler = CommandHandler(
-        port=5055
-    )
-    handler.send_command("test\n")
+    conf = Config()
+    try:
+        handler = CommandHandler(
+            port=conf.port,
+            hostname=conf.machine
+        )
+    except Exception as e:
+        print("Failed to connect to server: " + str(e))
+        sys.exit(84)
+    handler.send_command(conf.name)
     res0 = handler.receive_response()
     res1 = handler.receive_response()
     res2 = handler.receive_response()
     print("res0: '" + res0 + "'")
     print("res1: '" + res1 + "'")
     print("res2: '" + res2 + "'")
-
-
 
 if __name__ == "__main__":
     main()
