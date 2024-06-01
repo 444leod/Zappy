be3a0188c8460015eef7512aeacc984ae8e3e50e
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-06-01 17:49:56 +0200

    feat(Command-Class): add Inventory interpret result

--- ai/src/bot.py
+++ ai/src/bot.py
@@ -4,7 +4,37 @@ import sys
 from typing import List
 from connection_handler import ConnectionHandler
 from config import Config
+from enum import Enum
 import commands as cmd
+from dataclasses import dataclass, field
+
+class Orientation(Enum):
+    NORTH = 1
+    EAST = 2
+    SOUTH = 3
+    WEST = 4
+
+@dataclass
+class Inventory():
+    food: int = 10
+    linemate: int = 0
+    deraumere: int = 0
+    sibur: int = 0
+    mendiane: int = 0
+    phiras: int = 0
+    thystame: int = 0
+
+@dataclass
+class PlayerInfo():
+    level: int = 1
+    inv: Inventory = field(default_factory=Inventory)
+    pos: tuple[int, int] = (0, 0)
+    orientation: Orientation = Orientation.NORTH
+
+@dataclass
+class GeneralInfo():
+    map_size: tuple[int, int] = (0, 0)
+    nb_eggs: int = 0
 
 class Bot():
     def __init__(self, verbose=False, traced=False) -> None:
@@ -17,31 +47,34 @@ class Bot():
         except Exception as e:
             print("Failed to connect to server: " + str(e))
             sys.exit(84)
-        self.verbose: bool = verbose
-        self.traced: bool = traced
         self.results: List[str] = []
         self.results.append(self.com_handler.receive_response())
         if (self.results[-1] != "WELCOME\n"):
             print("Failed to connect to server: " + self.results[-1])
             sys.exit(84)
+
         self.com_handler.send_command(self.conf.name)
         self.results.append(self.com_handler.receive_response())
         self.results.append(self.com_handler.receive_response())
-        #tmp bc I'll have a class for relevant data later on
-        tmpNbEggs: int = int(self.results[-2])
-        tmpX, tmpY = map(int, self.results[-1].split())
-        self.log(f"{tmpNbEggs=}")
-        self.log(f"{tmpX=} {tmpY=}")
+
+        self.verbose: bool = verbose
+        self.traced: bool = traced
+
+        self.general_info: GeneralInfo = GeneralInfo()
+        self.general_info.map_size = tuple(map(int, self.results[-1].split()))
+        self.general_info.nb_eggs = int(self.results[-2])
+
+        self.player_info: PlayerInfo = PlayerInfo()
+
         self.messages_received: List[tuple[int, str]] = [] # [(playerID, message), ..]
         self.messages_sent: List[str] = []
         self.cmd_sent: List[str] = []
         self.base_funcs = {
             "dead\n" : self.die,
-            "message": self.receive_message  
+            "message": self.receive_broadcast  
         }
 
     def log(self, *args, **kargs) -> None:
-        # res: str = str(*args, **kargs)
         if (self.verbose):
             print(*args, **kargs)
         if (self.traced):
@@ -52,7 +85,7 @@ class Bot():
         self.log("Bot died. Exiting.")
         sys.exit(0)
 
-    def receive_message(self) -> None:
+    def receive_broadcast(self) -> None:
         tab: List[str] = self.results[-1].split(" ")
         if (len(tab) != 3) or not (tab[1][:-1].isdigit()):
             return
@@ -60,16 +93,25 @@ class Bot():
 
     def run(self) -> None:
         while True:
+            # Behavior logic here
+            cmd_to_send = cmd.Inventory()
+            self.cmd_sent.append(cmd_to_send.dump())
+            self.com_handler.send_command(cmd_to_send.dump())
+
+            # Wait for the response
             self.results.append(self.com_handler.receive_response())
             self.log(self.results[-1], end="")
+
             # Handle death or receiving a message by Broadcast
             key: str = self.results[-1].split(" ")[0]
             if (key in self.base_funcs):
                 self.base_funcs[key]()
-            # rest of logic here
-            # cmd_to_send = cmd.Forward()
-            # self.cmd_sent.append(cmd_to_send.dump())
-            # self.com_handler.send_command(cmd_to_send.dump())
+                continue
+
+            # Handle the command sent
+            if (self.cmd_sent[-1] == cmd.Inventory().dump()):
+                self.player_info.inv = Inventory(**(cmd_to_send.interpret_result(self.results[-1])))
+                self.log(self.player_info.inv)
 
 def main() -> None:
     bot = Bot(
--- ai/src/commands.py
+++ ai/src/commands.py
@@ -1,9 +1,17 @@
+import re
+
 class ACommand:
     def __init__(self, name: str) -> None:
         self.name = name
     
     def dump(self) -> str:
         return self.name
+    
+    def interpret_result(self, res: str) -> dict:
+        if (res == "ok\n"):
+            return {}
+        if (res == "ko\n"):
+            raise Exception(f"Failed to execute command: {res}")
 
 class Forward(ACommand):
     def __init__(self) -> None:
@@ -20,10 +28,18 @@ class Left(ACommand):
 class Look(ACommand):
     def __init__(self) -> None:
         super().__init__("Look")
+    
+    def interpret_result(self, res: str) -> dict:
+        super().interpret_result(res)
 
 class Inventory(ACommand):
     def __init__(self) -> None:
         super().__init__("Inventory")
+    
+    def interpret_result(self, res: str) -> dict:
+        super().interpret_result(res)
+        items = re.findall(r'(\w+)\s+(\d+)', res)
+        return {item: int(quantity) for item, quantity in items}
 
 class Broadcast(ACommand):
     def __init__(self, message: str) -> None:
