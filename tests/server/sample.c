80478c1c6d6184a9391ce434df8994270402d8a7
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-06-03 12:16:30 +0200

    docs(tests): added more sample & docs

--- tests/server/sample.c
+++ tests/server/sample.c
@@ -5,6 +5,7 @@
 ** sample
 */
 
+#include <unistd.h>
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
@@ -13,10 +14,28 @@ void init(void) {
     cr_redirect_stderr();
 }
 
-Test(example_suite, success_example, .init=init) {
+/**
+ * A boolean assertion example.
+ * You can simply check equalities this way.
+*/
+Test(example_suite, assert_example_one) {
+    cr_assert(0 == 0);
+}
+
+/**
+ * A typed assertion example.
+ * If you don't want to write `==`,
+ * then you can call `..._eq`.
+*/
+Test(example_suite, assert_eq_example) {
     cr_assert_eq(0, 0);
 }
 
-Test(example_suite, failure_example, .init=init) {
-    cr_assert_eq(0, 1);
+/**
+ * An assertion example based on program output.
+ * Uses `.init` to setup the redirection of standard outputs.
+*/
+Test(example_suite, stdout_example, .init=init) {
+    write(1, "Hello!", 6);
+    cr_assert_stdout_eq_str("Hello!");
 }
