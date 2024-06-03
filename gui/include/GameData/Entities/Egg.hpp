c82f8c4629ed11c8f97745a1c47340705f3d1539
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-03 14:58:41 +0200

    feat(Egg): add class for egg as entity

---
+++ gui/include/GameData/Entities/Egg.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** zappy
+** File description:
+** Egg
+*/
+
+#ifndef EGG_HPP_
+#define EGG_HPP_
+
+#include "AEntities.hpp"
+
+class Egg : public AEntities {
+    public:
+        Egg() = default;
+        ~Egg() = default;
+};
+
+#endif /* !EGG_HPP_ */
