36d096ad05378a3d34b59e53445b782ea825ada6
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-03 19:52:54 +0200

    fix(unused): remove unnecessary method and variable that are now in the map

--- gui/include/GameData/GameData.hpp
+++ gui/include/GameData/GameData.hpp
@@ -34,77 +34,6 @@ namespace gui {
             ~GameData() = default;
 
             /**
-             * @brief Get the map size
-             * @return Vector2u The map size
-            */
-            Vector2u mapSize() const { return this->_mapSize; }
-
-            /**
-             * @brief Set the map size
-             * @param mapSize The map size
-            */
-            void setMapSize(Vector2u mapSize) { this->_mapSize = mapSize; }
-
-            /**
-             * @brief Get the tile content at specific coordinates
-             * @param coordinates The coordinates of the tile
-             * @return TileContent The tile content
-             * @throw std::out_of_range If the coordinates are out of range
-            */
-            TileContent tileContent(Vector2u coordinates) const
-            {
-                auto it = this->_mapContent.find(coordinates);
-                if (it != this->_mapContent.end())
-                    return it->second;
-                else
-                    throw std::out_of_range("Coordinates out of range");
-            }
-
-            /**
-             * @brief Check if the coordinates are valid
-             * @param coordinates The coordinates
-             * @return bool True if the coordinates are valid, false otherwise
-            */
-            bool isValidCoordinates(Vector2u coordinates) const
-            {
-                return (coordinates.x() < this->_mapSize.x() && coordinates.y() < this->_mapSize.y());
-            }
-
-            /**
-             * @brief Set the tile content at specific coordinates
-             * @param coordinates The coordinates of the tile
-             * @param tileContent The new tile content
-             * @throw std::out_of_range If the coordinates are out of range
-            */
-            void setTileContent(Vector2u coordinates, TileContent tileContent)
-            {
-                if (!isValidCoordinates(coordinates))
-                    throw std::out_of_range("Coordinates out of range");
-
-                this->_mapContent[coordinates] = tileContent;
-            }
-
-            /**
-             * @brief Get the map content
-             * @return std::map<Vector2u, TileContent> The map content
-            */
-            std::map<Vector2u, TileContent> mapContent() const
-            {
-                return this->_mapContent;
-            }
-
-            /**
-             * @brief Set the map content
-             * @param mapContent The map content
-            */
-            void setMapContent(std::map<Vector2u, TileContent> mapContent)
-            {
-                for (auto &tile : mapContent) {
-                    this->_mapContent[tile.first] = tile.second;
-                }
-            }
-
-            /**
              * @brief Get the team names
              * @return std::vector<std::string> The team names
             */
@@ -115,12 +44,6 @@ namespace gui {
              * @param teamName The team name
             */
             void addTeamName(std::string teamName) { this->_teamNames.push_back(teamName); }
-
-            /**
-             * @brief Get the tile number
-             * @return int The tile number
-            */
-            std::uint32_t tileNbr() const { return (this->_mapSize.x() * this->_mapSize.y()); }
 
             /**
              * @brief Get the player
@@ -187,8 +110,6 @@ namespace gui {
             void addMessage(Message message) { this->_messages.push_back(message); }
 
         private:
-            Vector2u _mapSize = {0, 0};
-            std::map<Vector2u, TileContent> _mapContent = {};
             std::vector<std::string> _teamNames = {};
             std::vector<Character> _players = {};
             std::vector<Egg> _eggs = {};
