1d61a8cdaca7fd2de57b2099b27663ffcdd9cc1c
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-05 16:10:38 +0200

    fix(header): remove method of map of more than two lines, and put them in an appropriate file

---
+++ gui/src/GameData/Tiles/Map.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** zappy
+** File description:
+** Map
+*/
+
+#include "Tiles/Map.hpp"
+
+namespace gui {
+    TileContent Map::getTileContentByCoordinates(Vector2u coordinates) const
+    {
+        auto it = this->_mapContent.find(coordinates);
+        if (it != this->_mapContent.end())
+            return it->second;
+        else
+            throw std::out_of_range("Coordinates out of range");
+    }
+
+    void Map::setTileContent(Vector2u coordinates, TileContent tileContent)
+    {
+        if (!isValidCoordinates(coordinates))
+            throw std::out_of_range("Coordinates out of range");
+
+        this->_mapContent[coordinates] = tileContent;
+    }
+
+    void Map::setMapContent(std::map<Vector2u, TileContent> mapContent)
+    {
+        this->_mapContent.clear();
+
+        for (auto &tile : mapContent) {
+            this->_mapContent[tile.first] = tile.second;
+        }
+    }
+}
