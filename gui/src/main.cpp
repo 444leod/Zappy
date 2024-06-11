9c7e563cfae4b99d0018cd01ca7fdce55d1f15be
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 17:24:15 +0200

    feat(argument): change behavior of argument checking to unordered one

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -8,6 +8,9 @@
 #include "Handler/Command/GameDataManager.hpp"
 #include <cstring>
 
+/**
+ * @brief Print the usage
+ */
 void printUsage()
 {
     std::cout << "USAGE: ./zappy_gui -p port -h machine" << std::endl;
@@ -15,25 +18,57 @@ void printUsage()
     std::cout << "machine: hostname of the server" << std::endl;
 }
 
+/**
+ * @brief Check the flags
+ * @param args The arguments
+ * @return The flags in a map
+ */
+std::map<std::string, std::string> checkFlags(std::vector<std::string> args)
+{
+    int p_count = std::count(args.begin(), args.end(), "-p");
+    int h_count = std::count(args.begin(), args.end(), "-h");
+    std::map<std::string, std::string> flags;
+
+    if (p_count != 1 || h_count != 1)
+        throw std::invalid_argument("You only need one -p and one -h flag.");
+    for (auto it = args.begin(); it != args.end(); ++it) {
+        if (it->front() == '-') {
+            if (std::next(it) != args.end()) {
+                flags[it->substr(1)] = *std::next(it);
+            }
+        }
+    }
+    return flags;
+}
+
+/**
+ * @brief Check the arguments
+ * @param ac The number of arguments
+ * @param av The arguments
+ */
 void checkArgs(int ac, char **av)
 {
-    if (std::strcmp(av[1], "-help") == 0) {
-        printUsage();
-        return;
-    }
+    std::vector<std::string> args(av, av + ac);
+    std::map<std::string, std::string> flags;
+
     if (ac != 5) {
         printUsage();
         throw std::invalid_argument("Invalid number of arguments");
     }
-    if (std::strcmp(av[1], "-p") == 0) {
-        if (std::atoi(av[2]) < 1024 || std::atoi(av[2]) > 65535)
+    checkFlags(args);
+    flags = checkFlags(args);
+    if (flags.find("p") != flags.end()) {
+        std::string port = flags["p"];
+        if (port.find_first_not_of("0123456789") != std::string::npos)
+            throw std::invalid_argument("Port must be a number");
+        if (std::stoi(port) < 1024 || std::stoi(port) > 65535)
             throw std::invalid_argument("Port must be between 1024 and 65535");
-        if (std::strcmp(av[3], "-h") == 0) {
-            if (std::strlen(av[4]) < 1)
-                throw std::invalid_argument("Invalid hostname");
-        }
-    } else
-        throw std::invalid_argument("Invalid arguments, try using -help for more information");
+    }
+    if (flags.find("h") != flags.end()) {
+        std::string hostname = flags["h"];
+        if (hostname.empty())
+            throw std::invalid_argument("Hostname must not be empty");
+    }
 }
 
 int main(int ac, char **av)
