396b0b17a128f1a134401843b737c9519a063a36
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 19:21:08 +0200

    fix(interface): remove unused and unecessary Interface

--- gui/include/Handler/Command/ACommand.hpp
+++ gui/include/Handler/Command/ACommand.hpp
@@ -7,11 +7,13 @@
 
 #pragma once
 
-#include "ICommand.hpp"
 #include <sstream>
+#include <iostream>
+#include "Client.hpp"
+#include "GameData.hpp"
 
 namespace gui {
-    class ACommand : public ICommand {
+    class ACommand {
         public:
             virtual void stage(ntw::Client &client, std::string parameters = "") = 0;
             virtual void receive(std::string command, GameData &gameData) = 0;
