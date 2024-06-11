a98a5482935a70a164c77e1a1669ccc6170495e9
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 15:34:06 +0200

    fix(team nbr): remove variable not in command line, team number

--- gui/src/Handler/Command/CommandProtocol/Tna.cpp
+++ gui/src/Handler/Command/CommandProtocol/Tna.cpp
@@ -18,9 +18,8 @@ void gui::Tna::receive(std::string command, GameData &gameData)
     std::istringstream iss(command);
     std::string token;
     std::string teamName;
-    std::uint32_t teamCount;
 
-    iss >> token >> teamName >> teamCount;
-    for (std::uint32_t i = 0; i < teamCount; i++)
+    iss >> token >> teamName;
+    for (std::size_t i = 0; i < gameData.teamNames().size(); i++)
         gameData.teamNames().push_back(teamName);
 }
