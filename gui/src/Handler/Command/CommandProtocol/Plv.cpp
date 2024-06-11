b7a84375b80ccdb136e520d26e4d03d61515e099
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 15:14:24 +0200

    feat(error): add error handling for player level and id

--- gui/src/Handler/Command/CommandProtocol/Plv.cpp
+++ gui/src/Handler/Command/CommandProtocol/Plv.cpp
@@ -26,5 +26,10 @@ void gui::Plv::receive(std::string command, GameData &gameData)
     std::uint32_t level;
 
     iss >> token >> playerId >> level;
+
+    if (!gameData.playerExists(playerId))
+        throw std::runtime_error("Player does not exist in the game data.");
+    if (gameData.players().at(playerId)->playerLevel() < level || level > 8 || gameData.players().at(playerId)->playerLevel() > level)
+        throw std::runtime_error("Player level received does not match the player level in the game data.");
     // gameData.players().at(playerId)->increasePlayerLevel();
 }
