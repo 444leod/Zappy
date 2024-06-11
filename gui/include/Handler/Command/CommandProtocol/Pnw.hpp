cca624b62b726a2000512f9f4214e044a80f8005
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 13:37:30 +0200

    fix(method): chnage method from public to private

--- gui/include/Handler/Command/CommandProtocol/Pnw.hpp
+++ gui/include/Handler/Command/CommandProtocol/Pnw.hpp
@@ -15,14 +15,12 @@ namespace gui {
             void send(std::string command, ntw::Client &client) override;
             void receive(std::string command, GameData &gameData) override;
 
+        private:
             /**
              * @brief get the orientation based on a string
              * @param orientation The orientation
              * @return Orientation The orientation
             */
             Orientation getOrientationFromStr(std::string orientation);
-
-        protected:
-        private:
     };
 }
