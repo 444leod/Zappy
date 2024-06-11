6a4ad2ce32f8e9573f11256339c72c40a2aec8c3
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 16:13:05 +0200

    feat(error pix): add error handling for player coordinate, level, position

--- gui/src/Handler/Command/CommandProtocol/Pic.cpp
+++ gui/src/Handler/Command/CommandProtocol/Pic.cpp
@@ -23,7 +23,16 @@ void gui::Pic::receive(std::string command, GameData &gameData)
     std::uint32_t playerId;
 
     iss >> token >> x >> y >> level;
+
+    if (x >= gameData.mapRef().mapSize().x() || y >= gameData.mapRef().mapSize().y())
+        throw std::invalid_argument("Invalid tile coordinates, out of map bounds.");
     while (iss >> playerId) {
+        if (!gameData.playerExists(playerId))
+            throw std::runtime_error("Player does not exist");
+        if (gameData.players().at(playerId)->position().x() != x || gameData.players().at(playerId)->position().y() != y)
+            throw std::runtime_error("Player position does not match the position in the game data.");
+        if (gameData.players().at(playerId)->playerLevel() != level)
+            throw std::runtime_error("Player level does not match the level in the game data.");
         // gameData.players().at(playerId).setIsIncantating(true);
     }
 }
