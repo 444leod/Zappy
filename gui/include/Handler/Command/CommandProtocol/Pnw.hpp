58fc8b4f919b3af5036c5e8f3882e7fbdbae5158
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-11 16:32:47 +0200

    feat(doc): add doc for each command

--- gui/include/Handler/Command/CommandProtocol/Pnw.hpp
+++ gui/include/Handler/Command/CommandProtocol/Pnw.hpp
@@ -12,7 +12,18 @@
 namespace gui {
     class Pnw : public ACommand {
         public:
+            /**
+             * @brief Stage the command
+             * @param client The client
+             * @param parameters The parameters
+            */
             void stage(ntw::Client &client, std::string parameters = "") override;
+
+            /**
+             * @brief Receive the command
+             * @param command The command
+             * @param gameData The game data
+            */
             void receive(std::string command, GameData &gameData) override;
 
         private:
