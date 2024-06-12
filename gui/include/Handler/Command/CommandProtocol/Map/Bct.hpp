67a164454ef89f05e39b4b62ccd22197497c9fa3
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-12 16:49:21 +0200

    feat(doc): add better doc for map command

--- gui/include/Handler/Command/CommandProtocol/Map/Bct.hpp
+++ gui/include/Handler/Command/CommandProtocol/Map/Bct.hpp
@@ -13,15 +13,19 @@ namespace gui {
     class Bct : public ICommand {
         public:
             /**
-             * @brief Stage the command
+             * @brief The command Bct send the position of the tile to the server and expect the content of the tile
              * @param client The client
              * @param parameters The parameters
             */
             void stage(ntw::Client &client, std::string parameters = "") override;
 
             /**
-             * @brief Receive the command
-             * @param command The command
+             * @brief The command Bct receive the content of a tile
+             * @note Receive the command: bct X Y q0 q1 q2 q3 q4 q5 q6
+             * @note X Y -> x and y position
+             * @note q0 -> food
+             * @note q1 q2 q3 q4 q5 q6 -> rocks
+             * @param command The command given by the server
              * @param gameData The game data
             */
             void receive(std::string command, GameData &gameData) override;
--- gui/include/Handler/Command/CommandProtocol/Map/Msz.hpp
+++ gui/include/Handler/Command/CommandProtocol/Map/Msz.hpp
@@ -13,15 +13,17 @@ namespace gui {
     class Msz : public ICommand {
         public:
             /**
-             * @brief Stage the command
+             * @brief The command Msz send simply "msz" to the server and expect the map size
              * @param client The client
              * @param parameters The parameters
             */
             void stage(ntw::Client &client, std::string parameters = "") override;
 
             /**
-             * @brief Receive the command
-             * @param command The command
+             * @brief The command Msz receive the map size
+             * @note Receive the command: msz X Y
+             * @note X Y -> x and y size of the map
+             * @param command The command given by the server
              * @param gameData The game data
             */
             void receive(std::string command, GameData &gameData) override;
