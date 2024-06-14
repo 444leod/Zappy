ae48bb1ce6ebedc7054cda22b8adaa2d938838e7
Sacha Polerowicz <sacha.polerowicz@epitech.eu>
2024-06-14 12:01:21 +0200

    fix(mct): remove unused receive command

--- gui/src/Handler/Command/CommandProtocol/Map/Mct.cpp
+++ gui/src/Handler/Command/CommandProtocol/Map/Mct.cpp
@@ -15,26 +15,7 @@ void gui::Mct::stage(ntw::Client &client, std::string parameters)
 
 void gui::Mct::receive(std::string command, GameData &gameData)
 {
-    std::istringstream iss(command);
-    std::string token;
-    std::uint32_t x, y;
-    std::uint32_t linemate, deraumere, sibur, mendiane, phiras, thystame, food;
-
-    iss >> token >> x >> y >> food >> linemate >> deraumere >> sibur >> mendiane >> phiras >> thystame;
-    if (iss.fail())
-        throw std::invalid_argument("Invalid arguments");
-    if (x >= gameData.mapRef().mapSize().x() || y >= gameData.mapRef().mapSize().y())
-        throw std::invalid_argument("Invalid tile coordinates, out of map bounds.");
-    Vector2u coordinates(x, y);
-    TileContent tileContent;
-
-    tileContent.rocks().linemate.setQuantity(linemate);
-    tileContent.rocks().deraumere.setQuantity(deraumere);
-    tileContent.rocks().sibur.setQuantity(sibur);
-    tileContent.rocks().mendiane.setQuantity(mendiane);
-    tileContent.rocks().phiras.setQuantity(phiras);
-    tileContent.rocks().thystame.setQuantity(thystame);
-    tileContent.setFood(food);
-
-    gameData.mapRef().setTileContentByCoordinates(coordinates, tileContent);
+    (void)gameData;
+    (void)command;
+    std::cerr << "Command mct: can't be received." << std::endl;
 }
