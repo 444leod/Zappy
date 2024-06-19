2fad49f08919e8f11f25ccce87772bfdd818af29
Léo DUMONT <leo.dumont@epitech.eu>
2024-06-20 01:48:08 +0200

    feat(CommandHandler): added a command handler class to handle any command sent by the server correctly.

---
+++ gui/src/Core/CommandHandler.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** CommandHandler
+*/
+
+#include "CommandHandler.hpp"
+
+void gui::CommandHandler::handleCommand(std::string& command)
+{
+    static uint16_t wrongCommands = 0;
+
+    std::istringstream iss(command);
+    std::string token;
+    iss >> token;
+
+    if (token == "ok" || token == "ko")
+        return;
+    auto it = _responseHandlers.find(token);
+    if (it != _responseHandlers.end()) {
+        std::cout << "[DEBUG] Handling command: " << token << std::endl;
+        it->second->receive(command, _gameData);
+    } else {
+        wrongCommands++;
+        std::cerr << "Command not found: " << token << std::endl;
+        if (wrongCommands > 15) {
+            throw ntw::Client::ClientException("Too many wrong commands received. Exiting...");
+        }
+    }
+}
