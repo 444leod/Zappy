0800c0044c0cc4ed01961459d4fe993dd4e7779f
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-06-21 16:07:26 +0200

    feat(ZAP-46): smg function to send graphical client direct messages

--- server/src/commands/graphical/smg.c
+++ server/src/commands/graphical/smg.c
@@ -6,5 +6,14 @@
 */
 
 #include "commands.h"
+#include "commands_utils.h"
 #include "lib.h"
 #include "zappy.h"
+
+/**
+ * @brief Sends a mesage to the graphical clients
+ */
+void smg(const char *message)
+{
+    queue_to_graphical(my_snprintf("smg %s", message));
+}
