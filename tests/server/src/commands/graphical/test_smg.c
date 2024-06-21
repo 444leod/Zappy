3765282a5166c773976e855cec4e393d9116cfc8
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-06-21 16:32:00 +0200

    fix(test smg): randomly doubled

--- tests/server/src/commands/graphical/test_smg.c
+++ tests/server/src/commands/graphical/test_smg.c
@@ -21,17 +21,3 @@ Test(smg_suite, send_empty_message)
     assert_packet_queue(clientA->packet_queue, 1, "smg hehehe haw");
     assert_packet_queue(clientB->packet_queue, 1, "smg hehehe haw");
 }
-
-Test(smg_suite, send_empty_message)
-{
-    client_t clientA = test_create_client(0);
-    client_t clientB = test_create_client(1);
-
-    clientA->type = GRAPHICAL;
-    clientB->type = GRAPHICAL;
-
-    smg("hehehe haw");
-
-    assert_packet_queue(clientA->packet_queue, 1, "smg hehehe haw");
-    assert_packet_queue(clientB->packet_queue, 1, "smg hehehe haw");
-}
