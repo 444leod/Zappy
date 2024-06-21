73294b3edb3115210a101adbb9a4a9a6aae3d747
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-06-21 11:49:50 +0200

    fix(forward): not wrapping correctly

--- server/src/commands/ai/forward.c
+++ server/src/commands/ai/forward.c
@@ -11,6 +11,19 @@
 #include "clients.h"
 #include "lib.h"
 
+static position_t wrap_pos(position_t pos, const map_t map)
+{
+    if (pos.x < 0)
+        pos.x += map->width;
+    if (pos.y < 0)
+        pos.y += map->height;
+    if (pos.x >= (int)map->width)
+        pos.x -= map->width;
+    if (pos.y >= (int)map->height)
+        pos.y -= map->height;
+    return pos;
+}
+
 /**
  * @brief Forward command
  * @details Move the player forward
@@ -34,8 +47,7 @@ void forward(
     pos.y += client->player->orientation == SOUTH;
     pos.x += client->player->orientation == EAST;
     pos.x -= client->player->orientation == WEST;
-    pos.x %= server_info->map->width;
-    pos.y %= server_info->map->height;
+    pos = wrap_pos(pos, (const map_t)server_info->map);
     move_player(client->player, pos, server_info->map);
     add_packet_to_queue(&client->packet_queue, build_packet("ok"));
 }
