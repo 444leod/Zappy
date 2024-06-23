52bb303e5348c47c79f9cc7a54992f292c964804
ElliotKetchup <elliot.alladaye@epitech.eu>
2024-06-23 20:51:43 +0200

    test(testing): add functionnal tests to check cli

---
+++ tests/ai/argument_parsing.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Usage:
+
+"USAGE: ./zappy_ai -p port -n name [-h machine] [-m]"
+
+import subprocess
+import os
+
+SERVER_PORT = 5555
+TEAM_NAME_0 = "test0"
+TEAM_NAME_1 = "test1"
+
+
+if os.path.isfile('./zappy_ai') == False or os.path.isfile('./zappy_server') == False:
+    print("Executable not found in directory")
+    exit(1)
+
+server_process = subprocess.Popen(["./zappy_server", "-p", f"{SERVER_PORT}", "-x", "10", "-y", "10", "-n", f"{TEAM_NAME_0}", f"{TEAM_NAME_1}", "-c", "15", "-f", "50"])
+
+assert (subprocess.getstatusoutput('./zappy_ai'))[0] == 84, "No arguments passed, expected return code 84"
+print("No arguments passed, expected return code 84 passed")
+
+assert (subprocess.getstatusoutput('./zappy_ai -p'))[0] == 84, "No port passed, expected return code 84"
+print("No port passed, expected return code 84 passed")
+
+assert (subprocess.getstatusoutput(f'./zappy_ai -p {SERVER_PORT}'))[0] == 84, "No name passed, expected return code 84"
+print("No name passed, expected return code 84 passed")
+
+assert (subprocess.getstatusoutput(f'./zappy_ai -p {SERVER_PORT} -n'))[0] == 84, "No name passed, expected return code 84"
+print("No name passed, expected return code 84 passed")
+
+assert (subprocess.getstatusoutput(f'./zappy_ai -p {SERVER_PORT} -n {TEAM_NAME_0} -h'))[0] == 84, "No machine passed, expected return code 84"
+print("No machine passed, expected return code 84 passed")
+
+assert (subprocess.getstatusoutput(f'./zappy_ai -p {SERVER_PORT} -n GRAPHIC'))[0] == 84, "Name is GRAPHIC, expected return code 84"
+print("Name is GRAPHIC, expected return code 84 passed")
+
+#close server port
+os.kill(server_process.pid, 9)
